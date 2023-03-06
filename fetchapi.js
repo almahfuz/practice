a7e4e082b94dd87b0ac9565275640280d7772c69
almahfuz <abujafaralmahfuz@gmail.com>
2023-03-06 13:25:11 +0000

    show data from api

--- fetchapi.js
+++ fetchapi.js
@@ -1,5 +1,5 @@
 
-
+// fetch the data from users 
 function LoadData(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
@@ -11,13 +11,14 @@ function LoadData2(){
     .then(response => response.json())
     .then(data => console.log(data))
 }
-function LoadDataShow(){
+// show the data from users 
+function LoadDataShow(){ // this onclick function
     const url= 'https://jsonplaceholder.typicode.com/users';
     fetch(url)
     .then(response => response.json())
     .then(valueOfData => displayFromHere(valueOfData))
 }
-function displayFromHere(valueOfData){
+function displayFromHere(valueOfData){ // fetch the data and show data from here
     const  ul = document.getElementById('user-lists');
     for (let user of valueOfData) {
         console.log(user.name);
@@ -28,3 +29,5 @@ function displayFromHere(valueOfData){
         }
     
 }
+
+// post method using method 
