df98805ad92da14a2d6d4f72c98e735e621d5094
almahfuz <abujafaralmahfuz@gmail.com>
2023-03-06 13:18:46 +0000

    show data from api

--- fetchapi.js
+++ fetchapi.js
@@ -11,3 +11,20 @@ function LoadData2(){
     .then(response => response.json())
     .then(data => console.log(data))
 }
+function LoadDataShow(){
+    const url= 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+    .then(response => response.json())
+    .then(valueOfData => displayFromHere(valueOfData))
+}
+function displayFromHere(valueOfData){
+    const  ul = document.getElementById('user-lists');
+    for (let user of valueOfData) {
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li); 
+        
+        }
+    
+}
