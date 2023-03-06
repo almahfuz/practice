234d0b7372b8b2070848b18d2a40e9c16f97ce91
almahfuz <abujafaralmahfuz@gmail.com>
2023-03-06 14:37:00 +0000

    show data from api(post)

--- fetchapi.js
+++ fetchapi.js
@@ -31,3 +31,36 @@ function displayFromHere(valueOfData){ // fetch the data and show data from here
 }
 
 // post method using method 
+
+
+function LoadDataPost(){
+    const url= 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+    .then(response => response.json())
+    .then(valueOfDataPost => DataPostFormHere(valueOfDataPost))
+
+}
+function DataPostFormHere(valueOfDataPost){ // fetch the data and show data from here
+    const UsePostData = document.getElementById('User-Post-lists');
+    
+    for (const user of valueOfDataPost) {
+        // console.log(user.name);
+        const PostDiv  = document.createElement('div');
+        console.log(user);
+        PostDiv.classList.add('grooveColor');
+        PostDiv.innerHTML =`
+        <h5>All Posted people here</h5>
+        <h5>User Name : ${user.name}</h5> 
+        <p>User id : ${user.id}</p> 
+        <p>Email :${user.email} </p>
+        <p>phone Number:${user.phone } </p>
+        
+        
+        `;
+        UsePostData.appendChild(PostDiv);
+        
+        }
+    
+}
+
+
